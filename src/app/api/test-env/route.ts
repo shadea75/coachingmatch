87d278388226407dae9834dd167e63f33f7a5b84
shadea75 <debora.carofiglio@gmail.com>
2026-01-03 18:08:33 +0100

    route.ts

--- src/app/api/test-env/route.ts
+++ src/app/api/test-env/route.ts
@@ -1,1 +1,10 @@
+import { NextResponse } from 'next/server'
 
+export async function GET() {
+  return NextResponse.json({
+    hasClientId: !!process.env.GOOGLE_CLIENT_ID,
+    hasClientSecret: !!process.env.GOOGLE_CLIENT_SECRET,
+    clientIdPrefix: process.env.GOOGLE_CLIENT_ID?.substring(0, 10) + '...',
+    appUrl: process.env.NEXT_PUBLIC_APP_URL
+  })
+}
