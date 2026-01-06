bc35d79a5975da7a122a7fee787aa30cdd91c141
shadea75 <debora.carofiglio@gmail.com>
2026-01-06 19:55:16 +0100

    firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,1 +1,16 @@
-src/lib/firebase-admin.ts
+import { initializeApp, getApps, cert } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+
+const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n')
+
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      privateKey: privateKey,
+    }),
+  })
+}
+
+export const adminDb = getFirestore()
